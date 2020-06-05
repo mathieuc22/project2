2c158bd712f037f08c88f479240d33a2dbf56b36
Mathieu <mathieu.collet@gmail.com>
2020-06-06 00:19:17 +0200

    change stuff

--- static/index.js
+++ static/index.js
@@ -6,12 +6,15 @@ document.addEventListener('DOMContentLoaded', () => {
   // When connected, configure buttons
   socket.on('connect', () => {
     // Send content of the input text on submit
-    document.querySelector('#btn-username').onclick = () => {
+    document.querySelector('#form-username').onsubmit = () => {
       const username = document.querySelector('#username').value;
       const room = 'General';
       document.querySelector('#General').checked = true;
       //document.querySelector('#current-room').innerHTML = `Current room: ${room}`;
       socket.emit('join', {'username': username, 'room': room});
+
+      // Stop form from submitting
+      return false;
     };
 
     // Each button should emit a "submit vote" event
@@ -21,7 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
           const username = document.querySelector('#username').value;
           const room = button.dataset.room;
           console.log(room)
-          document.querySelector('#msg_history').innerHTML='';
+          document.querySelector('.msg_history').innerHTML='';
           socket.emit('join', {'username': username, 'room': room});
       };
     });
@@ -29,22 +32,25 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   // Send content of the input text on submit
-  document.querySelector('#send-msg').onclick = () => {
+  document.querySelector('#form-msg').onsubmit = () => {
     var msg = document.querySelector('#message').value;
     const room = document.querySelector('[name="options"]:checked').dataset.room;
     document.querySelector('#message').value= '';
     socket.emit('send message', {'msg': msg, 'room': room});
+
+    // Stop form from submitting
+    return false;
   };
 
   // When a new vote is announced, add to the unordered list
   socket.on('broadcast message', data => {
     console.log(`Broadcast in ${socket.rooms}`);
+
     const username = document.querySelector('#username').value;
-    const d0 = document.querySelector('#msg_history')
+    const d0 = document.querySelector('.msg_history');
     const d1 = document.createElement('div');
-    const dmsg = document.createElement('div');
-    const p = document.createElement('p');
-    const s = document.createElement('span');
+    //const s = document.createElement('span');
+
     var today = new Date();
     var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     var date = months[today.getMonth()]+' '+today.getDate();
@@ -52,26 +58,17 @@ document.addEventListener('DOMContentLoaded', () => {
     var dateTime = date+' | '+time;
 
     if (username != data.username) {
-      const d2 = document.createElement('div');
-      d1.setAttribute('class', 'incoming_msg');
-      d2.setAttribute('class', 'received_msg');
-      dmsg.setAttribute('class', 'received_withd_msg');
-      d0.appendChild(d1);
-      d1.appendChild(d2);
-      d2.appendChild(dmsg);
-      p.innerHTML = `${data.username}: ${data.msg}`;
+      d1.setAttribute('class', 'alert w-75 alert-dark');
+      d1.innerHTML = `${data.username}: ${data.msg}`;
     } else {
-      d1.setAttribute('class', 'outgoing_msg');
-      dmsg.setAttribute('class', 'sent_msg');
-      d0.appendChild(d1);
-      d1.appendChild(dmsg);
-      p.innerHTML = data.msg;
+      d1.setAttribute('class', 'alert w-75 ml-auto alert-info text-right');
+      d1.innerHTML = data.msg;
     }
 
-    dmsg.appendChild(p);
-    s.setAttribute('class', 'time_date');
-    s.innerHTML =dateTime;
-    dmsg.appendChild(s);
+    d0.appendChild(d1);
+    //s.setAttribute('class', 'time_date');
+    //s.innerHTML =dateTime;
+    //d1.appendChild(s);
 
     d0.scrollTop = d0.scrollHeight;
   });
